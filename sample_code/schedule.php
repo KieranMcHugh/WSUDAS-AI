bd0d3baa091d7b56f51917d128ce4bad87988ffe
Kieran McHugh <144867910+KieranMcHugh@users.noreply.github.com>
2025-11-05 22:44:22 -0800

    add schedule.php

---
+++ sample_code/schedule.php
@@ -0,0 +1,36 @@
+/* Note: this file is listed as "kernal.php" in the models codebase but changed here for legibility*/
+
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')->hourly();
+
+        // -----> added by Kieran
+        $schedule->command('scoutlabs:sync')->sundays()->at('12:00');
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
